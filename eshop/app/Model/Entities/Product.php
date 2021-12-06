afa134b7c6ebcba85224afe5e2cc468ac3beb2ad
sam96x <samuel@repka.org>
2021-12-06 19:07:35 +0100

    Vytvorenie entity pre produkty

---
+++ eshop/app/Model/Entities/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model\Entities;
+
+use LeanMapper\Entity;
+
+/**
+ * class Product
+ * @package App\Model\Entities
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $url
+ * @property string $photoExtension = ''
+ * @property float $price
+ * @property bool $available = true
+ * @property Category|null $category m:hasOne
+ */
+
+class Product extends Entity
+{
+
+}
